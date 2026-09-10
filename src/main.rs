26f52e6c1f050685b8d97d97f7e8e6872cd80a29
Dolbhi <110004436+Dolbhi@users.noreply.github.com>
2026-09-10 20:35:00 +1000

    no more close confirmation

--- src/main.rs
+++ src/main.rs
@@ -39,33 +39,33 @@ impl eframe::App for MyApp {
             ui.heading("Central Panel");
         });
 
-        if ui.input(|i| i.viewport().close_requested()) {
-            if self.allowed_to_close {
-                // do nothing - we will close
-            } else {
-                ui.send_viewport_cmd(egui::ViewportCommand::CancelClose);
-                self.show_confirmation_dialog = true;
-            }
-        }
+        // if ui.input(|i| i.viewport().close_requested()) {
+        //     if self.allowed_to_close {
+        //         // do nothing - we will close
+        //     } else {
+        //         ui.send_viewport_cmd(egui::ViewportCommand::CancelClose);
+        //         self.show_confirmation_dialog = true;
+        //     }
+        // }
 
-        if self.show_confirmation_dialog {
-            egui::Window::new("Do you want to quit?")
-                .collapsible(false)
-                .resizable(false)
-                .show(ui.ctx(), |ui| {
-                    ui.horizontal(|ui| {
-                        if ui.button("No").clicked() {
-                            self.show_confirmation_dialog = false;
-                            self.allowed_to_close = false;
-                        }
+        // if self.show_confirmation_dialog {
+        //     egui::Window::new("Do you want to quit?")
+        //         .collapsible(false)
+        //         .resizable(false)
+        //         .show(ui.ctx(), |ui| {
+        //             ui.horizontal(|ui| {
+        //                 if ui.button("No").clicked() {
+        //                     self.show_confirmation_dialog = false;
+        //                     self.allowed_to_close = false;
+        //                 }
 
-                        if ui.button("Yes").clicked() {
-                            self.show_confirmation_dialog = false;
-                            self.allowed_to_close = true;
-                            ui.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                });
-        }
+        //                 if ui.button("Yes").clicked() {
+        //                     self.show_confirmation_dialog = false;
+        //                     self.allowed_to_close = true;
+        //                     ui.send_viewport_cmd(egui::ViewportCommand::Close);
+        //                 }
+        //             });
+        //         });
+        // }
     }
 }
