e863905d1a976037dfac655527c31b1ba1586c49
Dolbhi <110004436+Dolbhi@users.noreply.github.com>
2026-09-10 23:32:45 +1000

    rough layout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use eframe::egui;
-use egui::Id;
-use egui_commonmark;
+use egui::{Color32, Id, Layout};
 
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -19,76 +18,47 @@ fn main() -> eframe::Result {
 
 #[derive(Default)]
 struct MyApp {
-    // show_confirmation_dialog: bool,
-    // allowed_to_close: bool,
     text_box_text: String,
 }
 
 impl eframe::App for MyApp {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
-        egui::Panel::bottom(Id::new("bottom_1")).show(ui, |ui| {
-            ui.heading("Bottom Panel");
-            ui.text_edit_multiline(&mut self.text_box_text);
+        egui::Panel::right(Id::new("right")).exact_size(200.0).show(ui, |ui| {
+            ui.heading("Side panel");
         });
 
-        egui::Panel::bottom(Id::new("bottom_2")).show(ui, |ui| {
-            ui.heading("Bottom Panel 2");
-            // ui.label(format!("The box says: {}", self.text_box_text));
-            let mut cache = egui_commonmark::CommonMarkCache::default();
-            egui_commonmark::CommonMarkViewer::new().show(ui, &mut cache, self.text_box_text.as_str());
-        });
-
-        egui::CentralPanel::default().show(ui, |ui| {
-            ui.heading("Central Panel");
-            ui.vertical_centered(|ui| {
-                let mut frame = egui::Frame::default().inner_margin(4.0).begin(ui);
-                {
-                    let response = frame.content_ui.label("Inside the frame 1");
-                    if response.hovered() {
-                        frame.frame.fill = egui::Color32::RED;
-                    }
-                }
-                frame.end(ui); // Will "close" the frame.
-                let mut frame = egui::Frame::default().inner_margin(4.0).begin(ui);
-                {
-                    let response = frame.content_ui.label("Inside the frame 2");
-                    if response.hovered() {
-                        frame.frame.fill = egui::Color32::GREEN;
-                    }
-                }
-                frame.end(ui); // Will "close" the frame.
+        egui::Panel::bottom(Id::new("bottom")).exact_size(120.0).show(ui, |ui| {
+            ui.heading("Input");
+            ui.with_layout(Layout::top_down_justified(egui::Align::Center), |ui| {
+                egui::TextEdit::multiline(&mut self.text_box_text).hint_text("Math here").show(ui);
             });
         });
 
-        
+        egui::CentralPanel::default().show(ui, |ui| {
+            ui.heading("Output");
+            ui.label(format!("The box says: {}", self.text_box_text));
+        });
 
-        // if ui.input(|i| i.viewport().close_requested()) {
-        //     if self.allowed_to_close {
-        //         // do nothing - we will close
-        //     } else {
-        //         ui.send_viewport_cmd(egui::ViewportCommand::CancelClose);
-        //         self.show_confirmation_dialog = true;
-        //     }
-        // }
-
-        // if self.show_confirmation_dialog {
-        //     egui::Window::new("Do you want to quit?")
-        //         .collapsible(false)
-        //         .resizable(false)
-        //         .show(ui.ctx(), |ui| {
-        //             ui.horizontal(|ui| {
-        //                 if ui.button("No").clicked() {
-        //                     self.show_confirmation_dialog = false;
-        //                     self.allowed_to_close = false;
-        //                 }
-
-        //                 if ui.button("Yes").clicked() {
-        //                     self.show_confirmation_dialog = false;
-        //                     self.allowed_to_close = true;
-        //                     ui.send_viewport_cmd(egui::ViewportCommand::Close);
-        //                 }
-        //             });
-        //         });
-        // }
+        // egui::CentralPanel::default().show(ui, |ui| {
+        //     ui.heading("Central Panel");
+        //     ui.vertical_centered(|ui| {
+        //         let mut frame = egui::Frame::default().inner_margin(4.0).begin(ui);
+        //         {
+        //             let response = frame.content_ui.label("Inside the frame 1");
+        //             if response.hovered() {
+        //                 frame.frame.fill = egui::Color32::RED;
+        //             }
+        //         }
+        //         frame.end(ui); // Will "close" the frame.
+        //         let mut frame = egui::Frame::default().inner_margin(4.0).begin(ui);
+        //         {
+        //             let response = frame.content_ui.label("Inside the frame 2");
+        //             if response.hovered() {
+        //                 frame.frame.fill = egui::Color32::GREEN;
+        //             }
+        //         }
+        //         frame.end(ui); // Will "close" the frame.
+        //     });
+        // });
     }
 }
